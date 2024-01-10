4195b668e85efdf431ebf826b3e981848b028bb2
Omollo Alphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-10 09:45:59 +0300

    Instances using constructors

--- Classes/intro.py
+++ Classes/intro.py
@@ -7,15 +7,17 @@ class Robot:
   def introduce_self(self):
     print('My name is ' + self.name)
 
-r1 = Robot()# create a new object from the class Robot
-r1.name = "Eva"
-r1.color = "Brown"
-r1.weight = 3
+#r1 = Robot()# create a new object from the class Robot
+#r1.name = "Eva"
+#r1.color = "Brown"
+#r1.weight = 3
+r1 = Robot("Eva", "Brown", 3)
+r2 = Robot("Chui", "White", 4.5)
 
-r2 = Robot()
-r2.name = "Chui"
-r2.color = "White"
-r2.weight = 4.5
+#r2 = Robot()
+#r2.name = "Chui"
+#r2.color = "White"
+#r2.weight = 4.5
 
 r1.introduce_self() #  prints my name is Eva
 r2.introduce_self() # prints my name is Chui
