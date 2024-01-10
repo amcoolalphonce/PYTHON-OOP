3fb2adce3c94b8411356225ff49dd2aa2f1db56b
Omollo Alphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-10 09:18:24 +0300

    Instance and attributes

--- Classes/intro.py
+++ Classes/intro.py
@@ -1,3 +1,7 @@
 class Robot:
-  def introduce_self():
+  def introduce_self(self):
     print('My name is ' + self.name)
+
+r1 = Robot()# create a new object from the class Robot
+r1.name = "Eva"
+r1.color = "Brown"
