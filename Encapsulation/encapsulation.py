4f99e58b038bf450d78439384077af36430597db
Omollo Alphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-11 13:01:42 +0300

    Update encapsulation.py

--- Encapsulation/encapsulation.py
+++ Encapsulation/encapsulation.py
@@ -4,23 +4,18 @@ class Car:
     self.__color = color
   #to prevent modificatio when acccess is granted to other people
   def set_Speed(self, value):
-    self.speed = value
+    self.__speed = value
   def set_Speed(self):
-    return self.speed
+    return self.__speed
 
 
 
 v8 = Car(240, "Black")
 lexus = Car(190, "Grey")
+v8.set_speed(320)
 
-v8.set_speed(300)
-print(v8.get_speed())
-            
-# to chnage any value of the attributtes
-v8.speed = 300 #new v8 speed is 300 from 240
-
-
-print(v8.speed)
+print(v8.__speed) # returns 320
+print(v8.color) #no attribute named color in car
 print(lexus.speed)
             
             
