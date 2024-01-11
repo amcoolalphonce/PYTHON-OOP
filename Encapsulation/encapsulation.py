d159359451187fb0567ae288955e1da9396e39a6
Omollo Alphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-11 12:44:07 +0300

    Update encapsulation.py

--- Encapsulation/encapsulation.py
+++ Encapsulation/encapsulation.py
@@ -2,3 +2,13 @@ class Car:
   def __init__(self, speed, color):
     self.speed = speed
     self.color = color
+
+
+# to chnage any value of the attributtes
+
+v8 = Car(240, "Black")
+lexus = Car(190, "Grey"
+print(v8.speed)
+print(lexus.speed)
+            
+            
