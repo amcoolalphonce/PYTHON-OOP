489ba1718e69ed9256ba8c1ff2cb897dfc3fd3f9
Omollo Alphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-12 15:24:23 +0300

    Rectangle class

--- Classes/circleArea.py
+++ Classes/circleArea.py
@@ -8,7 +8,10 @@ class Circle:
     return round(math.pi * self.radius **2, 2)
 
 
+class Rectangle:
+  def __init__(self, length, width):
 #usage
 circle_1 = Circle(42)
 print(circle_1.radius)# 42
 print(circle_1.calculate_area()) #5541.77
+
