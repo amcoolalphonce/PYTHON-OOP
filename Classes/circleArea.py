4e27e6aa7bd21cdfa506f49e0e81755c244421ce
Omollo Alphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-12 15:38:49 +0300

    Update circleArea.py

--- Classes/circleArea.py
+++ Classes/circleArea.py
@@ -19,6 +19,8 @@ class Rectangle:
 #usage
 circle_1 = Circle(42)
 rectangle1 = Rectangle(40, 20)
-print(circle_1.radius)# 42
-print(circle_1.calculate_area()) #5541.77
+print(f'Width of rectangle1 is: {rectangle1.width} , length is {rectangle1.length}')
+print(f'Area of rectangle is : {rectangle1.rectangle_area()}')
+print('Circle area is :', circle_1.radius)# 42
+print('Circle area is : ' , circle_1.calculate_area()) #5541.77
 
